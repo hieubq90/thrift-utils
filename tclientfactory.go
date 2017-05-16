454077d1cc3fe74fce13a558d6b1605fc4e90722
hieubq <hieubq@vng.com.vn>
2017-05-17 01:09:35 +0700

    update code

---
+++ tclientfactory.go
@@ -0,0 +1,86 @@
+package thrift_utils
+
+import (
+	"fmt"
+
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/hieubq90/go-commons-pool"
+)
+
+type TProtocolType uint8
+
+const (
+	BINARY_PROTOCOL TProtocolType = iota + 1
+	COMPACT_PROTOCOL
+)
+
+//
+type MakeObjectFunction func(t thrift.TTransport, f thrift.TProtocolFactory) interface{}
+
+type TClientFactory struct {
+	tag              string
+	endpoint         string
+	protocolFactory  thrift.TProtocolFactory
+	transportFactory thrift.TTransportFactory
+	makeObjectFunc   MakeObjectFunction
+}
+
+func NewTClientFactory(tag, endpoint string, fn MakeObjectFunction, pType TProtocolType) *TClientFactory {
+	if pType == BINARY_PROTOCOL {
+		return &TClientFactory{
+			tag:              tag,
+			endpoint:         endpoint,
+			protocolFactory:  thrift.NewTBinaryProtocolFactory(true, true),
+			transportFactory: thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory()),
+		}
+	} else {
+		return &TClientFactory{
+			tag:              tag,
+			endpoint:         endpoint,
+			protocolFactory:  thrift.NewTCompactProtocolFactory(),
+			transportFactory: thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory()),
+		}
+	}
+}
+
+// MakeObject function use to create new thrift client when needed
+func (f *TClientFactory) MakeObject() (*pool.PooledObject, error) {
+
+	var transport thrift.TTransport
+	var err error
+
+	transport, err = thrift.NewTSocket(f.endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	transport = f.transportFactory.GetTransport(transport)
+	err = transport.Open()
+	if err != nil {
+		fmt.Println(f.tag+" | error on opening connection to "+f.endpoint, err)
+		return nil, err
+	}
+
+	tClient := f.makeObjectFunc(transport, f.protocolFactory)
+	return pool.NewPooledObject(tClient), nil
+}
+
+func (f *TClientFactory) DestroyObject(object *pool.PooledObject) error {
+	//do destroy
+	return nil
+}
+
+func (f *TClientFactory) ValidateObject(object *pool.PooledObject) bool {
+	//do validate
+	return true
+}
+
+func (f *TClientFactory) ActivateObject(object *pool.PooledObject) error {
+	//do activate
+	return nil
+}
+
+func (f *TClientFactory) PassivateObject(object *pool.PooledObject) error {
+	//do passivate
+	return nil
+}
---
+++ tclientwrapper.go
@@ -0,0 +1,90 @@
+package thrift_utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hieubq90/go-commons-pool"
+)
+
+type TClientWrapper struct {
+	tag          string
+	objPool      *pool.ObjectPool
+	maxTryTimes  int
+	reTryTimeout int
+}
+
+func (cw *TClientWrapper) GetClient() interface{} {
+	if cw.objPool != nil {
+		for try := 1; try <= cw.maxTryTimes; try++ {
+			fmt.Printf("%s | borrow client from pool | [%d]\n", cw.tag, try)
+			client, err := cw.objPool.BorrowObject()
+			if err != nil {
+				// co the sleep
+				fmt.Printf("%s | no more client to borrow | [%d]\n", cw.tag, try)
+				time.Sleep(time.Millisecond * time.Duration(cw.reTryTimeout))
+			} else {
+				return client
+			}
+		}
+	}
+	return nil
+}
+
+func (cw *TClientWrapper) ReturnClient(client interface{}) (err error) {
+	fmt.Printf("%s | return client to pool\n", cw.tag)
+	if cw.objPool != nil {
+		err = cw.objPool.ReturnObject(client)
+	}
+	return
+}
+
+func (cw *TClientWrapper) DeleteClient(client interface{}) (err error) {
+	fmt.Printf("%s | delete client of pool\n", cw.tag)
+	if cw.objPool != nil {
+		cw.objPool.Destroy(client)
+	}
+	return
+}
+
+func NewDefaultPoolConfig() (cfg *pool.ObjectPoolConfig) {
+	cfg = pool.NewDefaultPoolConfig()
+	cfg.MaxTotal = 8
+	cfg.MaxIdle = 8
+	cfg.MinIdle = 0
+	cfg.MinEvictableIdleTimeMillis = int64(7200)
+	cfg.TimeBetweenEvictionRunsMillis = int64(7200)
+	cfg.TestOnBorrow = false
+	return cfg
+}
+
+func NewPoolConfig(poolSize, maxIdle, minIdle int, evictionTime int64) (cfg *pool.ObjectPoolConfig) {
+	cfg = pool.NewDefaultPoolConfig()
+	cfg.MaxTotal = poolSize
+	cfg.MaxIdle = maxIdle
+	cfg.MinIdle = minIdle
+	cfg.MinEvictableIdleTimeMillis = evictionTime
+	cfg.TimeBetweenEvictionRunsMillis = evictionTime
+	cfg.TestOnBorrow = false
+	return cfg
+}
+
+func NewTClientWrapperWithDefaultConfig(tag string, f *TClientFactory) (cw *TClientWrapper) {
+	cw = &TClientWrapper{
+		tag:          tag,
+		maxTryTimes:  5,
+		reTryTimeout: 500,
+		objPool:      pool.NewObjectPool(f, NewDefaultPoolConfig()),
+	}
+	return cw
+}
+
+func NewTClientWrapper(tag string, f *TClientFactory, maxTry, reTryTimeout int, cfg *pool.ObjectPoolConfig) (cw *TClientWrapper) {
+	cw = &TClientWrapper{
+		tag:          tag,
+		maxTryTimes:  maxTry,
+		reTryTimeout: reTryTimeout,
+		objPool:      pool.NewObjectPool(f, cfg),
+	}
+	return cw
+}
